0c51fd8d0f5a8925e17e00d533d15a22e97d5c9d
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-06-17 09:39:39 +0530

    Time: 60 ms (22.8%), Space: 18.7 MB (22.69%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    //vector<vector<int>>dp(coins.size(), vector<int> (amount, 0));
+    int dfs(int amount,vector<int>&coins,int i,int sum,vector<vector<int>>&dp){
+        if(sum==amount){
+            return 1;
+        }
+        if(sum>amount){
+            return 0;
+        }
+        if(i==coins.size()){
+            return 0;
+        }
+        if (dp[i][sum]!=-1) {
+            return dp[i][sum];
+        }
+        return dp[i][sum] = dfs(amount, coins, i, sum + coins[i],dp)
+                     + dfs(amount, coins, i + 1, sum,dp);
+        
+       
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>>dp(coins.size(), vector<int> (amount, -1));
+        return dfs(amount,coins,0,0,dp);
+    }
+};
