216be5db7ff75376ccf054c4727bf481c00203f8
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-06-25 11:02:35 +0530

    Time: 162 ms (91.44%), Space: 8.7 MB (62.98%) - LeetHub

--- 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
+++ 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     int MOD = 1e9+7;
     
-    int memo[101][201];
+    int dp[101][201];
     int n;
     
-    int dfs(vector<int>& A, int finish, int cur, int cur_fuel){
-        if(cur_fuel < 0){
+    int dfs(vector<int>& locations, int finish, int start, int fuel){
+        if(fuel < 0){
             return 0;
-        }else if(memo[cur][cur_fuel] != -1){
-            return memo[cur][cur_fuel];
+        }else if(dp[start][fuel] != -1){
+            return dp[start][fuel];
         }else{
-            //cur_fuel >= 0
-            int ways = (cur == finish);
             
-            if(cur_fuel > 0){
-                for(int next = 0; next < n; ++next){
-                    if(next == cur) continue;
-                    ways = (ways +
-                        dfs(A, finish, next, cur_fuel - abs(A[cur]-A[next]))) % MOD;
+            int ans = (start == finish);
+            
+            if(fuel > 0){
+                for(int i = 0; i < n; ++i){
+                    if(i == start) continue;
+                    ans = (ans +dfs(locations, finish, i, fuel - abs(locations[start]-locations[i]))) % MOD;
                 }
             }
             
-            return memo[cur][cur_fuel] = ways;
+            return dp[start][fuel] = ans;
         }
     };
     
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
         n = locations.size();
         
-        memset(memo, -1, sizeof(memo));
+        memset(dp, -1, sizeof(dp));
         
         return dfs(locations, finish, start, fuel);
     }
