f0bb263cc8aecc491af8c5bdaf35de1258ef665e
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-07-03 13:08:35 +0530

    Time: 178 ms (22.67%), Space: 12.1 MB (51.77%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // bool compare(const std::vector<int>& a, const std::vector<int>& b) {
+    //     return (a[0] > b[0] ||(a[0]==b[0] && a[1]<b[1]));
+    // }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        vector<vector<int>>result;
+        sort(people.begin(),people.end(),[](const vector<int>& a, const vector<int>& b){
+            return (a[0] > b[0] ||(a[0]==b[0] && a[1]<b[1]));
+        });
+        for(vector<int>&x:people){
+            result.insert(result.begin()+x[1],x);
+        }
+        return result;
+    }
+};
