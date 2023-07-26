8686b0445f69f95ef159cf9a2463c49eb36a27af
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-07-26 08:49:02 +0530

    Time: 5 ms (60.94%), Space: 7.4 MB (45.98%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int next(vector<int>&nums,int i){
+        int n=nums.size();
+        return (n+nums[i]+i)%n;
+    }
+
+    bool circularArrayLoop(vector<int>& nums) {
+        int n=nums.size();
+        for(int &num:nums){
+            num%=n;
+        }
+        for(int i=0;i<n;i++){
+            int slow=i,fast=i;
+            while(nums[slow]*nums[next(nums,fast)]>0 && nums[slow]*nums[next(nums,next(nums,fast))]>0 ){
+                slow=next(nums,slow);
+                fast=next(nums,next(nums,fast));
+                if(slow==fast){
+                    if(slow==next(nums,slow)){
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            int j=i;
+            int val=nums[i];
+            while(nums[j]*val>0){
+                int nexx=next(nums,j);
+                nums[j]=0;
+                j=nexx;
+            }
+        }
+        return false;
+    }
+};
