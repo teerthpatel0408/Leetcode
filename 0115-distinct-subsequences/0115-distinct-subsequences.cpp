a7c01d1b6079ec1a71619eac6588c0108f9db1b7
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-08-01 18:18:52 +0530

    Time: 41 ms (76.74%), Space: 30.1 MB (26.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,52 @@
 class Solution {
 public:
-    map<pair<int, int>, int> dp;
-    int dfs(string &s,string &t,int i, int j){
-        if(j==t.size()){
+int mod=1e9+7;
+    int helper(string s1,string s2,int ind1,int ind2,vector<vector<int>>&dp)
+    {
+        if(ind2<0)
+        {
             return 1;
         }
-        if(i==s.size()){
+        if(ind1<0)
+        {
             return 0;
         }
-        if (dp.find({i, j}) != dp.end()) {
-            return dp[{i, j}];
+        if(dp[ind1][ind2]!=-1)
+        {
+            return dp[ind1][ind2];
         }
-        
-        if (s[i] == t[j]) {
-            dp[{i, j}] = dfs(s, t, i + 1, j + 1)+dfs(s,t,i+1,j) ;
-        } else {
-            dp[{i, j}] = dfs(s, t, i + 1, j);
+        if(s1[ind1]==s2[ind2])
+        {
+            return dp[ind1][ind2]= helper(s1,s2,ind1-1,ind2-1,dp)+helper(s1,s2,ind1-1,ind2,dp);
         }
-        return dp[{i, j}];
-
+        else
+        {
+            return dp[ind1][ind2]= helper(s1,s2,ind1-1,ind2,dp);
+        }
     }
-    int numDistinct(string s, string t) {
-        return dfs(s,t,0,0);
+    int numDistinct(string s, string t) 
+    {
+        int n=s.length();
+        int m=t.length();
+        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int ind1=1;ind1<=n;ind1++)
+        {
+            for(int ind2=1;ind2<=m;ind2++)
+            {
+                if(s[ind1-1]==t[ind2-1])
+                {
+                    dp[ind1][ind2]= ((dp[ind1-1][ind2-1])%mod+(dp[ind1-1][ind2])%mod)%mod;
+                }
+                else
+                {
+                    dp[ind1][ind2]= (dp[ind1-1][ind2])%mod;
+                }
+            }
+        }
+        return (dp[n][m])%mod;
     }
 };
