66f098822283b4562236d8b2a79929bee9a07858
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-08-04 08:46:48 +0530

    Time: 7 ms (73.42%), Space: 13.4 MB (33.73%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>word_set(wordDict.begin(),wordDict.end());
+        int n=s.size();
+        vector<bool>dp(n+1,0);
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            if(!dp[i])continue;
+            for(int j=i+1;j<=n;j++){
+                if( word_set.count(s.substr(i,j-i)))
+                dp[j]=1;
+            }
+        }
+        return dp[n];
+    }
+    
+};
