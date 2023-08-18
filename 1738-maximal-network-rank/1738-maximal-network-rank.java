4bbb29c5f3ff5d076d591bf49a4d7b3c74dd7f6f
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-08-18 09:31:31 +0530

    Time: 2 ms (100%), Space: 44.8 MB (8.04%) - LeetHub

---
+++ 1738-maximal-network-rank/1738-maximal-network-rank.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        
+        int[] degrees = new int[n];
+        for (int[] road : roads) {
+            degrees[road[0]]++;
+            degrees[road[1]]++;
+        }
+        int firstMax = 0, secondMax = 0;
+        for (int degree : degrees) {
+            if (degree > firstMax) {
+                secondMax = firstMax;
+                firstMax =  degree;
+            }
+            else if (degree > secondMax && degree < firstMax) {
+                secondMax = degree;
+            }
+        }
+        
+        int fmCount = 0, smCount = 0;
+        for (int degree : degrees) {
+            if (degree == firstMax) fmCount++;
+            if (degree == secondMax) smCount++;
+        }
+        
+        
+        if (fmCount > 1) {
+            
+            int edgeCount = 0;
+            for (int[] road : roads){
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == firstMax) edgeCount++;
+            }
+            int maxEdgeWithFirstMax = fmCount * (fmCount-1) / 2;
+            
+            return 2 * firstMax - (maxEdgeWithFirstMax == edgeCount ? 1 : 0);
+        }
+        
+        
+        else {
+            
+            int edgeCount = 0;
+            for (int[] road : roads) {
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == secondMax) edgeCount++;
+                if (degrees[road[0]] == secondMax && degrees[road[1]] == firstMax) edgeCount++;
+            }
+            
+            
+            return firstMax + secondMax - (smCount == edgeCount ? 1 : 0);
+        }
+    }
+}
