bde8d22caa12815813fdce87db977a50a6293a6b
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-08-27 18:20:17 +0530

    Time: 4 ms (71.43%), Space: 5.9 MB (71.43%) - LeetHub

---
+++ 3026-find-the-minimum-possible-sum-of-a-beautiful-array/3026-find-the-minimum-possible-sum-of-a-beautiful-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long minimumPossibleSum(int n, int target) {
+        int half=target/2;
+        if(n<=half){
+            return n*1LL *(n+1)/2;
+        }
+        return half*1LL *(half+1)/2 +(n-half)*1LL *target+(n-half)*1LL*(n-half-1)/2;
+    }
+};
