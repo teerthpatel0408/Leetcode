e4ce36928e38aee0fa57ebdfe71f449091777766
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2023-09-12 10:02:26 +0530

    Time: 51 ms (74.86%), Space: 17.3 MB (77.52%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        int cnt[26] = {}, ans = 0;
+        unordered_set<int> used;
+        for (char ch : s)
+            ++cnt[ch - 'a'];
+        for (int i = 0; i < 26; ++i){
+            for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i]){
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
