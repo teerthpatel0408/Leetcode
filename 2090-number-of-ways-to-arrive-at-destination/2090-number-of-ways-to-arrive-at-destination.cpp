c96f1d331097a87c11c1b91d9de39cf7a4de2f1a
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-07-02 09:25:08 +0530

    Time: 63 ms (35.77%), Space: 39.3 MB (8.35%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<long long,long long>>>graph(n+1);
-        for(auto x:roads){
-            graph[x[0]].push_back({x[1],x[2]});
-            graph[x[1]].push_back({x[0],x[2]});
-        } 
-        long long inf=1e15;
-        long long mod=1e9+7;
-        vector<long long> dp(n+1,0);
-        vector<long long> dist(n+1,inf);
-        priority_queue<pair<long long , long long>>q;
-        dist[0]=0;
-        dp[0]=1;
-        q.push({0,0});
-
-        while(q.size()){
-            long long node=q.top().second;
-            long long nodewt=-q.top().first;
+        vector<vector<pair<long long,long long>>>adj(n);
+        for(auto it:roads)
+        {
+            long long x = it[0];
+            long long y = it[1];
+            long long wt = it[2];
+            pair<long long,long long> tx = {y,wt};
+            pair<long long,long long> ty = {x,wt};
+            adj[x].push_back(tx);
+            adj[y].push_back(ty);
+        }
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>q;//This stores time taken to reach a particuar node + the nodeval
+        vector<long long>dist(n,1e15);
+        vector<long long>ways(n,0);
+        dist[0] = 0;//distance to reach a start node from start is 0.
+        ways[0] = 1;//number of ways to reach start is 1.
+        q.push({0,0});//Queue will be storing the time and the nodeval
+        long long modu = 1e9+7;
+        while(!q.empty())
+        {
+            long long node = q.top().second;
+            long long timeTillNow = q.top().first;
             q.pop();
-            if(dist[node]<nodewt){
-                continue;
-            }
-            for(auto key:graph[node]){
-                long long child=key.first;
-                long long childwt=key.second;
-                if(nodewt+childwt == dist[child]){
-                    dp[child]=(dp[child]+dp[node])%mod;
+            for(auto it:adj[node])
+            {
+                long long val = it.first;
+                long long time  = it.second;
+                if(timeTillNow + time < dist[val])
+                {
+                    dist[val] = timeTillNow + time;
+                    ways[val] = ways[node];
+                    q.push({timeTillNow + time,val});
                 }
-                if(nodewt+childwt < dist[child]){
-                    dp[child]=dp[node]%mod;
-                    dist[child]=nodewt+childwt;
-                    q.push({-dist[child],child});
+                else if(timeTillNow + time == dist[val])
+                {
+                    ways[val] = (ways[val]+ways[node]) % modu;
                 }
             }
         }
-        return dp[n-1];
+        return ways[n-1]%modu;
     }
 };
