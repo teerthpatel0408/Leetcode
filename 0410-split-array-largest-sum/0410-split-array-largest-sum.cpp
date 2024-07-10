05a84f4e082630e3ad14c04e1de4022a5d8fad49
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-07-10 15:04:42 +0530

    Time: 0 ms (100%), Space: 9.1 MB (33.79%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int end = accumulate(nums.begin(), nums.end(), 0);
-        int start = *max_element(nums.begin(), nums.end()); // Corrected line
-        // int ans = INT_MAX;
-        while (start <= end) {
-            int mid = start + (end - start) / 2; 
+    int helper(vector<int>&nums,int k)
+    {
+        int end = accumulate(nums.begin(),nums.end(),0);
+        int start = *min_element(nums.begin(),nums.end());
+        int ans = INT_MAX;
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
             int countSegs = 1;
             int sum = 0;
-            // int miniSum = INT_MIN;
-            for (int num : nums) {
-                if (sum + num <= mid) {
-                    sum += num;
-                } else {
+            int miniSum = INT_MIN;
+            for(int i = 0;i<nums.size();i++)
+            {
+                if(sum + nums[i] <= mid)
+                {
+                    sum += nums[i];
+                }
+                else
+                {
                     countSegs++;
-                    // miniSum = max(miniSum, sum);
-                    sum = num;
+                    miniSum = max(miniSum,sum);
+                    sum = nums[i];
                 }
             }
-            // miniSum = max(miniSum, sum);
-            if (countSegs > k) {
+            miniSum = max(miniSum,sum);
+            if(countSegs > k)
+            {
                 start = mid + 1;
-            } else {
-                // ans = min(ans, miniSum);
+            }
+            else
+            {
+                // if(countSegs == k)
+                // {
+                    ans= min(ans,miniSum);
+                // }
                 end = mid - 1;
             }
-            // cout<<countSegs<<" "<<ans<<endl;
         }
-       
-        return start;
+        return ans;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        
+        return helper(nums,k);
     }
 };
