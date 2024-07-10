96a733017b2d04169d025033769e4a5a19ffee65
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-07-10 14:53:19 +0530

    Time: 0 ms (100%), Space: 9 MB (71.89%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -3,29 +3,31 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int end = accumulate(nums.begin(), nums.end(), 0);
         int start = *max_element(nums.begin(), nums.end()); // Corrected line
-        int ans = INT_MAX;
+        // int ans = INT_MAX;
         while (start <= end) {
-            int mid = start + (end - start) / 2; // More precise way to avoid potential overflow
+            int mid = start + (end - start) / 2; 
             int countSegs = 1;
             int sum = 0;
-            int miniSum = INT_MIN;
+            // int miniSum = INT_MIN;
             for (int num : nums) {
                 if (sum + num <= mid) {
                     sum += num;
                 } else {
                     countSegs++;
-                    miniSum = max(miniSum, sum);
+                    // miniSum = max(miniSum, sum);
                     sum = num;
                 }
             }
-            miniSum = max(miniSum, sum);
+            // miniSum = max(miniSum, sum);
             if (countSegs > k) {
                 start = mid + 1;
             } else {
-                ans = min(ans, miniSum);
+                // ans = min(ans, miniSum);
                 end = mid - 1;
             }
+            // cout<<countSegs<<" "<<ans<<endl;
         }
-        return ans;
+       
+        return start;
     }
 };
