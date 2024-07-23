b28f1c6844e09dd964d7600a009e5d8694def997
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-07-23 12:00:48 +0530

    Time: 234 ms (95.97%), Space: 133.7 MB (62.23%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -0,0 +1,35 @@
+class TimeMap {
+public:
+    unordered_map<string,vector<pair<int,string>>>tp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        tp[key].push_back({timestamp,value});
+    }
+    int bs(vector<pair<int,string>>&tp,int num)
+    {
+        int start = 0;
+        int end = tp.size()-1;
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            if(tp[mid].first<=num)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+    string get(string key, int timestamp) {
+        if(tp.find(key) == tp.end())    return "";
+        auto &curr = tp[key];
+        int num = bs(curr,timestamp);
+        return num == -1 ? "" : curr[num].second;
+    }
+};
