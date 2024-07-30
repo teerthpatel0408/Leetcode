b963e64c6378caeef14a0c026fdbc0969a9f8176
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-07-30 16:23:45 +0530

    Time: 1036 ms (83.26%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+(select name as results from MovieRating join Users on Users.user_id=MovieRating.user_id group by name order by count(*) desc, name limit 1)
+union all
+(select title as results from MovieRating join Movies on MovieRating.movie_id = Movies.movie_id where extract(year_month from created_at)=202002
+group by title order by avg(rating) desc,title limit 1)
