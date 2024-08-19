8d73fd6879698eead1cf3ea49a8d4ac8f16d21bc
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-08-19 10:41:14 +0530

    Time: 2 ms (76.41%), Space: 7.5 MB (55.97%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int ans=0;
+        for(int d=2;d<=n;d++){
+            while(n%d==0){
+                ans+=d;
+                n/=d;
+            }
+        }
+        return ans;
+    }
+};
