c4fdaa332f6e2151f81ad8a6ae43577591662274
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-08-21 09:08:01 +0530

    Time: 60 ms (26.73%), Space: 11.3 MB (37.94%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> dp;
-    
-    // Helper function to find the minimum number of operations required to print substring s[i...j]
-    int f(int i, int j, string& s){       
-        // If dp[i][j] has already been calculated, return its value to avoid recomputation.
+    int f(int i, int j, string& s,vector<vector<int>>&dp){       
         if (dp[i][j]!=-1) return dp[i][j];
         
-        // initial case: When i=j, it only takes 1 operation to print it.
-        if (i==j) return dp[i][j]=1;
+        if (i==j) return 1;
         
-        int ans; // Variable to store the minimum number of operations required to print substring s[i...j].
-        
-        // If s[i]==s[j] or s[j]==s[j-1],
-        // then it's equivalent to printing substring s[i...j-1].
-        if (s[i]==s[j]||s[j-1]==s[j]) ans=f(i, j-1, s);
-        
-        // If s[i]==s[i+1], it's equivalent to printing substring s[i+1...j].
-        else if (s[i]==s[i+1]) ans=f(i+1, j, s);
-        
-        // If the above two conditions are not met, it means that s[i] and s[j] are different, and s[j] is also different 
-        // from the character before it. In this case, we have multiple options:
-        else{
-            // Option 1: Print s[i...j-1] and then the character at position j, so the number of operations is f(i,j-1,s) + 1.
-            ans=f(i,j-1, s)+1;
-            
-            // Option 2: We can try to find a character k between i and j (exclusive) such that s[k] is the same as s[j].
-            // If we find such a character, it means we can print s[i...k-1] and s[k...j-1] together as one operation,
-            // since both sequences will end with the same character s[j]. The number of operations would be f(i,k-1,s) + f(k,j-1,s).
-            // We try all possible k values and choose the minimum among them.
-            for(int k=i+1; k<j; k++){
-                if(s[k]==s[j])
-                    ans=min(ans, f(i, k-1, s)+f(k, j-1, s));
-            }
+        int ans=INT_MAX; 
+        for(int k=i;k<j;k++){
+            ans=min(ans,f(i,k,s,dp)+f(k+1,j,s,dp));
         }
-        // Save the computed result in dp table to avoid recomputation for the same subproblems.
-        return dp[i][j]=ans;
+        return dp[i][j]=(s[i]==s[j])?ans-1:ans;
     }
     
     int strangePrinter(string s) {
-        n=s.size(); // Get the size of the input string.
-        dp.assign(n, vector<int>(n, -1)); // Initialize the dp table with -1 to mark that no subproblem has been computed yet.
-        return f(0, n-1, s); // Call the helper function to find the minimum number of operations to print the entire string.
+        int n=s.size(); 
+        vector<vector<int>>dp(n,vector<int>(n,-1)); 
+        return f(0, n-1, s,dp); 
     }
 };
