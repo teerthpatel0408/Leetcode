4f93cd3f0bba6a8c6e060a43fb99f02e2e338bc6
teerthpatel0408 <111279820+teerthpatel0408@users.noreply.github.com>
2024-11-30 17:25:36 +0530

    Time: 1554 ms (24.28%), Space: 301.6 MB (63.39%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
@@ -6,12 +6,12 @@ public:
         map<int,int>indegree;
         for(auto &p:pairs){
             adj[p[0]].push_back(p[1]);
-            ++indegree[p[0]];
-            --indegree[p[1]];
+            --indegree[p[0]];
+            ++indegree[p[1]];
         }
         int start=-1;
         for(auto &it:indegree){
-            if(it.second>0){
+            if(it.second<0){
                 start=it.first;
             }
         }
